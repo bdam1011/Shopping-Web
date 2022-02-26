c3396ccc256d68659420a473078f79443f628525
Harry Wang <harrywang111@gmail.com>
2022-02-26 13:42:58 +0800

    try add AppHeader to AboutView

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <AppHeader/>
   </div>
 </template>
+<script>
+import AppHeader from "@/components/AppHeader.vue";
+
+export default {
+  name: "AboutView",
+  components: {
+    AppHeader,
+  },
+};
+</script>
