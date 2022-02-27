940fcda534d30f632f81ab685e39b2bc58fb6fa6
Harry Wang <harrywang111@gmail.com>
2022-02-27 15:29:05 +0800

    add nav bar

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,16 +1,5 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <AppHeader/>
   </div>
 </template>
-<script>
-import AppHeader from "@/components/AppHeader.vue";
-
-export default {
-  name: "AboutView",
-  components: {
-    AppHeader,
-  },
-};
-</script>
