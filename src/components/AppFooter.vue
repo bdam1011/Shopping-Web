618757309e608f5261aebcc6b54447af3b7a74b1
Harry Wang <harrywang111@gmail.com>
2022-03-02 16:53:20 +0800

    add footer

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,40 @@
+<template>
+  <ul class="router-inlineblock">
+    <li><router-link to="/about">免責聲明</router-link></li>
+    <li><router-link to="/about">關於我們</router-link></li>
+    <li><router-link to="/about">常見問題</router-link></li>
+  </ul>
+</template>
+<script>
+export default {
+  name: "AppFooter",
+};
+</script>
+<style scoped>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  /* overflow: hidden; */
+  background-color: #38444d;
+}
+.router-inlineblock>li{
+  display: inline-block;
+  text-align: justify;
+  padding: 10px 5%;
+}
+li:hover {
+    background-color: rgb(115, 115, 115);
+    color: white;
+}
+a {
+  color: white;
+  display: block; /* 讓 <a> 填滿 <li> */
+  font-size: 1.5rem;
+  text-decoration: none;
+}
+
+a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
