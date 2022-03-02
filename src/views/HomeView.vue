4b41af1d6470fe7b41831e9b220f28c2258c65b8
Harry Wang <harrywang111@gmail.com>
2022-03-02 16:53:01 +0800

    add SearchBox

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <search-box/>
     <slider-carousel/>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
@@ -9,12 +10,14 @@
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
 import SliderCarousel from "@/components/Slider.vue";
+import SearchBox from "@/components/SearchBox.vue";
 
 export default {
   name: 'HomeView',
   components: {
     HelloWorld,
-    SliderCarousel
+    SliderCarousel,
+    SearchBox
   }
 }
 </script>
